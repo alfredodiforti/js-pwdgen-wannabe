239a39cefc4ff7972e977d98562a8dd1c5192d73
Alfredo Di Forti <adf.palermo@gmail.com>
2020-10-13 16:14:22 +0200

    Esercizio Completato

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Commento nome
+var name = prompt('come ti chiami?');
+document.getElementById('name').innerHTML= name;
+// Commento Cognome
+var surname = prompt('quale è il tuo cognome?');
+document.getElementById('surname').innerHTML= surname;
+// Commento Colore Preferito
+var color = prompt('Quale è il tuo colore preferito?');
+document.getElementById('color').innerHTML= color;
